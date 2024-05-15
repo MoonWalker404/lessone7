9e0b33cfe2049b2c8da4d36a5f40a9a97a47f744
MoonWalker404 <crossbeat71@gmail.com>
2024-05-15 19:51:16 +0300

    add new code in part4

--- part4.py
+++ part4.py
@@ -1,10 +1,27 @@
-class SmartHouse():
+# class SmartHouse():
+#     def turn_on_light(self):
+#         pass
+#
+#     def heat_food(self):
+#         pass
+#
+#     def turn_on_music(self):
+#         pass
+# Это 4 принцип SOLID 👆
+# Принцип разделения интерфейсов (ISP, Interface Segregation Principle)
+
+
+class Light():
     def turn_on_light(self):
-        pass
+        print("Свет включен")
 
+
+class Food():
     def heat_food(self):
-        pass
+        print("Еда начала разогреваться")
 
+
+class Music():
     def turn_on_music(self):
-        pass
+        print("Включаю подборку ваших любимых песен")
 
