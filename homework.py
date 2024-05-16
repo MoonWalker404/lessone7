24ec9254ffedde61e061cff0d3f54433eb57f067
MoonWalker404 <crossbeat71@gmail.com>
2024-05-16 15:20:19 +0300

    add - homework

---
+++ homework.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+
+class Weapon(ABC):
+    @abstractmethod
+    def attack(self):
+        pass
+
+class Sword(Weapon):
+    def attack(self):
+        print("Удар мечом")
+
+class Bow(Weapon):
+    def attack(self):
+        print("Выстрел луком")
+
+class Fighter:
+    def __init__(self, name):
+        self.name = name
+        self.weapon = None
+
+    def changeWeapon(self, weapon):
+        self.weapon = weapon
+
+    def attack(self):
+        if self.weapon:
+            return f"{self.name} наносит {self.weapon.attack()}"
+        else:
+            return f"{self.name} - нету оружия"
+
+class Monster:
+    def __init__(self, name):
+        self.name = name
+
+    def defeate(self):
+        return f"{self.name} - побежден"
+
+if __name__ == "__main__":
+    fighter = Fighter("Боец")
+    monster = Monster("Монстр")
+    sword = Sword()
+    bow = Bow()
+    fighter.changeWeapon(sword)
+    print("Боец выбирает меч")
+    print(fighter.attack())
+    print(monster.defeate())
+    fighter.changeWeapon(bow)
+    print("Боец выбирает kлук")
+    print(fighter.attack())
+    print(monster.defeate())
+
+
